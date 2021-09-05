96dee6ed9b123d0845f1ddfae861c5c4a906decc
Pranav <pranav.2002tis@gmail.com>
2021-09-05 15:34:29 +0530

    added images

--- src/components/DesignDoorsProducts.js
+++ src/components/DesignDoorsProducts.js
@@ -1,6 +1,8 @@
 import React from "react";
 import DD1 from "../images/design1.jpeg";
 import DD2 from "../images/design2.jpeg";
+import DD3 from "../images/design3.jpg";
+import DD4 from "../images/design4.jpg";
 
 function DesignDoorsProducts() {
   return (
@@ -27,6 +29,36 @@ function DesignDoorsProducts() {
         <div className="d-inline-flex px-4 my-5 mx-2 bd-highlight">
           <div className="card" style={{ width: 288 }}>
             <img src={DD1} className="card-img-top" alt="PVC Designer Doors" />
+            <div className="card-body">
+              <h2 className="card-title">
+                <strong>Designs Vol - 3</strong>
+              </h2>
+              <p className="card-text lead">
+                Applicable to both light and heavy quality
+              </p>
+              <h4 className="bestseller-heading">Bestseller</h4>
+            </div>
+          </div>
+        </div>
+
+        <div className="d-inline-flex px-4 my-5 mx-2 bd-highlight">
+          <div className="card" style={{ width: 288 }}>
+            <img src={DD3} className="card-img-top" alt="PVC Designer Doors" />
+            <div className="card-body">
+              <h2 className="card-title">
+                <strong>Designs Vol - 4</strong>
+              </h2>
+              <p className="card-text lead">
+                Applicable to both light and heavy quality
+              </p>
+              <h4 className="bestseller-heading">Bestseller</h4>
+            </div>
+          </div>
+        </div>
+
+        <div className="d-inline-flex px-4 my-5 mx-2 bd-highlight">
+          <div className="card" style={{ width: 288 }}>
+            <img src={DD4} className="card-img-top" alt="PVC Designer Doors" />
             <div className="card-body">
               <h2 className="card-title">
                 <strong>Designs Vol - 2</strong>
